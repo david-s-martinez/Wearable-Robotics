b373478e6775359639858ec1b91b9e801814b294
David S. Martinez <56695799+davidmartinez13@users.noreply.github.com>
2022-12-07 23:56:03 +0100

    added main_exo_control with equations

--- tutorial_3/main_exo_control.cpp
+++ tutorial_3/main_exo_control.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
-
+#include <iostream>
+#include <cmath>
 double deg2rad(double degree){
     return (degree * 3.14159265359/180);
 }
@@ -56,8 +57,8 @@ int main( int argc, char** argv )
     s.str("");
     s<<ns;
     ros::param::get(s.str(),g);
-
-    double tao = 0; 
+    // torque tau
+    double tau = 0; 
     
     //init params 
     double q1 = deg2rad(90); 
@@ -76,15 +77,15 @@ int main( int argc, char** argv )
 
     while(ros::ok())
     {        
-        m_matrix = //TODO
+        m_matrix = I233 + ((pow(L2,2) * m2)/4);
         c_matrix = -1.5*L1*L2*m2*sin(q1)*qd1;
-        g_matrix = //TODO 
-        b_matrix = //TODO
+        g_matrix = (-L2*gx*m2*sin(q1))/2 + (L2*gy*m2*cos(q1))/2 - k1*(theta1-q1);
+        b_matrix = b1*qd1;
         
         // calculate qdd1 and integrate 
-        qdd1=(tao//TODO)/m_matrix;
-        qd1 = delta_t*qdd1 + qd1;
-        q1 = // TODO;
+        qdd1=(tao - b_matrix*qd1 - g_matrix - c_matrix*qd1)/m_matrix;
+        qd1 = delta_t *qdd1 + qd1;
+        q1 = delta_t *qd1 + q1;
 
         // ROS_WARN_STREAM("qdd1"<<qdd1);
         // ROS_WARN_STREAM("qd1"<<qd1);
